5a9cc5422efe1209f2c5c38b312aae21443b4a78
zhuzijian <zhuzijian@meituan.com>
2015-04-10 02:17:24 +0800

    222

--- app/src/main/java/com/example/cameratest/CameraPreview.java
+++ app/src/main/java/com/example/cameratest/CameraPreview.java
@@ -13,148 +13,155 @@ import android.hardware.Camera.PictureCallback;
 import android.hardware.Camera.ShutterCallback;
 import android.view.SurfaceHolder;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class CameraPreview implements SurfaceHolder.Callback,
-		Camera.PreviewCallback {
+        Camera.PreviewCallback {
 
-	private int PreviewSizeWidth;
-	private int PreviewSizeHeight;
-	private Camera mCamera;
-	private boolean TakePicture;
-	private String NowPictureFileName;
-	private SurfaceHolder mSurfHolder;
+    private int PreviewSizeWidth;
+    private int PreviewSizeHeight;
+    private Camera mCamera;
+    private boolean TakePicture;
+    private String NowPictureFileName;
+    private SurfaceHolder mSurfHolder;
+    Bitmap mBitMap;
 
-	public CameraPreview(int PreviewlayoutWidth, int PreviewlayoutHeight) {
-		PreviewSizeWidth = PreviewlayoutWidth;
-		PreviewSizeHeight = PreviewlayoutHeight;
-	}
 
-	@Override
-	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
-		// TODO Auto-generated method stub
-		Parameters parameters;
-		mSurfHolder = arg0;
+    public CameraPreview(int PreviewlayoutWidth, int PreviewlayoutHeight) {
+        PreviewSizeWidth = PreviewlayoutWidth;
+        PreviewSizeHeight = PreviewlayoutHeight;
+    }
 
-		parameters = mCamera.getParameters();
-		// Set the camera preview size
-		parameters.setPreviewSize(PreviewSizeWidth, PreviewSizeHeight);
-		// Set the take picture size, you can set the large size of the camera
-		// supported.
-		parameters.setPictureSize(PreviewSizeWidth, PreviewSizeHeight);
+    @Override
+    public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+        // TODO Auto-generated method stub
+        Parameters parameters;
+        mSurfHolder = arg0;
 
-		// Turn on the camera flash.
-		String NowFlashMode = parameters.getFlashMode();
-		if (NowFlashMode != null)
-			parameters.setFlashMode(Parameters.FLASH_MODE_ON);
-		// Set the auto-focus.
-		String NowFocusMode = parameters.getFocusMode();
-		if (NowFocusMode != null)
-		parameters.setFocusMode("auto");
+        parameters = mCamera.getParameters();
+        // Set the camera preview size
+        parameters.setPreviewSize(PreviewSizeWidth, PreviewSizeHeight);
+        // Set the take picture size, you can set the large size of the camera
+        // supported.
+        parameters.setPictureSize(PreviewSizeWidth, PreviewSizeHeight);
 
-		mCamera.setParameters(parameters);
+        // Turn on the camera flash.
+        String NowFlashMode = parameters.getFlashMode();
+        if (NowFlashMode != null)
+            parameters.setFlashMode(Parameters.FLASH_MODE_ON);
+        // Set the auto-focus.
+        String NowFocusMode = parameters.getFocusMode();
+        if (NowFocusMode != null)
+            parameters.setFocusMode("auto");
 
-		mCamera.startPreview();
-	}
+        mCamera.setParameters(parameters);
 
-	@Override
-	public void surfaceCreated(SurfaceHolder arg0) {
-		// TODO Auto-generated method stub
-		mCamera = Camera.open(0);
-		try {
-			// If did not set the SurfaceHolder, the preview area will be black.
-			mCamera.setPreviewDisplay(arg0);
-			mCamera.setPreviewCallback(this);
-		} catch (IOException e) {
-			mCamera.release();
-			mCamera = null;
-		}
-	}
+        mCamera.startPreview();
+    }
 
-	@Override
-	public void surfaceDestroyed(SurfaceHolder arg0) {
-		// TODO Auto-generated method stub
-		mCamera.setPreviewCallback(null);
-		mCamera.stopPreview();
-		mCamera.release();
-		mCamera = null;
-	}
+    @Override
+    public void surfaceCreated(SurfaceHolder arg0) {
+        // TODO Auto-generated method stub
+        mCamera = Camera.open(0);
+        try {
+            // If did not set the SurfaceHolder, the preview area will be black.
+            mCamera.setPreviewDisplay(arg0);
+            mCamera.setPreviewCallback(this);
+        } catch (IOException e) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
 
-	@Override
-	public void onPreviewFrame(byte[] arg0, Camera arg1) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void surfaceDestroyed(SurfaceHolder arg0) {
+        // TODO Auto-generated method stub
+        mCamera.setPreviewCallback(null);
+        mCamera.stopPreview();
+        mCamera.release();
+        mCamera = null;
+    }
 
-	// Take picture interface
-	public void CameraTakePicture(String FileName) {
-		TakePicture = true;
-		NowPictureFileName = FileName;
-		mCamera.autoFocus(myAutoFocusCallback);
-	}
+    @Override
+    public void onPreviewFrame(byte[] arg0, Camera arg1) {
+        // TODO Auto-generated method stub
 
-	// Set auto-focus interface
-	public void CameraStartAutoFocus() {
-		TakePicture = false;
-		mCamera.autoFocus(myAutoFocusCallback);
-	}
+    }
 
-	AutoFocusCallback myAutoFocusCallback = new AutoFocusCallback() {
-		public void onAutoFocus(boolean arg0, Camera NowCamera) {
-			if (TakePicture) {
-				NowCamera.stopPreview();// fixed for Samsung S2
-				NowCamera.takePicture(shutterCallback, rawPictureCallback,
-						jpegPictureCallback);
-				TakePicture = false;
-			}
-		}
-	};
-	ShutterCallback shutterCallback = new ShutterCallback() {
-		public void onShutter() {
-			// Just do nothing.
-		}
-	};
+    // Take picture interface
+    public void CameraTakePicture(String FileName) {
+        TakePicture = true;
+        NowPictureFileName = FileName;
+        mCamera.autoFocus(myAutoFocusCallback);
+    }
 
-	PictureCallback rawPictureCallback = new PictureCallback() {
-		public void onPictureTaken(byte[] arg0, Camera arg1) {
-			// Just do nothing.
-		}
-	};
+    // Set auto-focus interface
+    public void CameraStartAutoFocus() {
+        TakePicture = true;
+        mCamera.autoFocus(myAutoFocusCallback);
+    }
 
-	PictureCallback jpegPictureCallback = new PictureCallback() {
-		public void onPictureTaken(byte[] data, Camera arg1) {
-			// Save the picture.
-			try {
-				Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0,
-						data.length);
-				FileOutputStream out = new FileOutputStream(NowPictureFileName);
+    AutoFocusCallback myAutoFocusCallback = new AutoFocusCallback() {
+        public void onAutoFocus(boolean arg0, Camera NowCamera) {
+            if (TakePicture) {
+                NowCamera.stopPreview();// fixed for Samsung S2
+                NowCamera.takePicture(shutterCallback, rawPictureCallback,
+                        jpegPictureCallback);
+                TakePicture = false;
+            }
+        }
+    };
+    ShutterCallback shutterCallback = new ShutterCallback() {
+        public void onShutter() {
+            // Just do nothing.
+        }
+    };
 
-                Bitmap bitmap2 = toGrayscale(bitmap);
-				
-				bitmap2.compress(Bitmap.CompressFormat.JPEG, 90, out);
-				
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	};
-	
-	public Bitmap toGrayscale(Bitmap bmpOriginal)
-	{        
-	    int width, height;
-	    height = bmpOriginal.getHeight();
-	    width = bmpOriginal.getWidth();    
+    PictureCallback rawPictureCallback = new PictureCallback() {
+        public void onPictureTaken(byte[] arg0, Camera arg1) {
+            // Just do nothing.
+        }
+    };
 
-	    Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-	    Canvas c = new Canvas(bmpGrayscale);
-	    Paint paint = new Paint();
-	    ColorMatrix cm = new ColorMatrix();
-	    cm.setSaturation(0);
-	    ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
-	    paint.setColorFilter(f);
-	    c.drawBitmap(bmpOriginal, 0, 0, paint);
-	    return bmpGrayscale;
-	}
+    PictureCallback jpegPictureCallback = new PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera arg1) {
+            // Save the picture.
+
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0,
+                    data.length);
+            FileOutputStream out = null;
+            try {
+                out = new FileOutputStream(NowPictureFileName);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            Bitmap bitmap2 = toGrayscale(bitmap);
+
+            bitmap2.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            mBitMap = bitmap2;
+
+
+
+        }
+    };
+
+    public Bitmap toGrayscale(Bitmap bmpOriginal) {
+        int width, height;
+        height = bmpOriginal.getHeight();
+        width = bmpOriginal.getWidth();
+
+        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmpGrayscale);
+        Paint paint = new Paint();
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        paint.setColorFilter(f);
+        c.drawBitmap(bmpOriginal, 0, 0, paint);
+        return bmpGrayscale;
+    }
 
 }
--- app/src/main/java/com/example/cameratest/MyCamera.java
+++ app/src/main/java/com/example/cameratest/MyCamera.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -14,6 +15,7 @@ import android.view.SurfaceView;
 import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -27,6 +29,7 @@ import com.github.mikephil.charting.data.LineDataSet;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MyCamera extends Activity {
@@ -34,12 +37,15 @@ public class MyCamera extends Activity {
     private CameraPreview camPreview;
     private LinearLayout mainLayout;
 
-    private Handler mHandler = new Handler(Looper.getMainLooper());
+    private Handler mHandler;
+
+    ArrayList<LineDataSet> dataSets;
+    ArrayList<String> xVals;
+    LineChart chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
         //Set this SPK Full screen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -49,7 +55,7 @@ public class MyCamera extends Activity {
         setContentView(R.layout.activity_main);
 
         SurfaceView camView = new SurfaceView(this);
-        SurfaceHolder camHolder = camView.getHolder();
+        final SurfaceHolder camHolder = camView.getHolder();
         camPreview = new CameraPreview(640, 480);
 
         camHolder.addCallback(camPreview);
@@ -57,8 +63,25 @@ public class MyCamera extends Activity {
 
         mainLayout = (LinearLayout) findViewById(R.id.linearLayout1);
         mainLayout.addView(camView, new LayoutParams(640, 480));
-
+        chart = (LineChart) findViewById(R.id.chart);
         initChart();
+        mHandler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                switch (msg.what) {
+                    case 1:
+                        mainLayout.removeAllViews();
+                        ImageView v = new ImageView(MyCamera.this);
+                        if(camPreview.mBitMap == null){
+                            Toast.makeText(MyCamera.this,"fdsafdsafa",Toast.LENGTH_SHORT).show();
+                        }
+                        v.setImageBitmap(camPreview.mBitMap);
+                        mainLayout.addView(v);
+                }
+            }
+        };
+
     }
 
     @Override
@@ -92,24 +115,29 @@ public class MyCamera extends Activity {
         return true;
     }
 
-    ;
-
     private Runnable TakePicture = new Runnable() {
         String extStorageDirectory = Environment.getExternalStorageDirectory().toString();
         String MyDirectory_path = extStorageDirectory;
         String PictureFileName;
 
+
         public void run() {
             File file = new File(MyDirectory_path);
             if (!file.exists())
                 file.mkdirs();
-            Toast.makeText(MyCamera.this,MyDirectory_path,Toast.LENGTH_SHORT).show();
+            Toast.makeText(MyCamera.this, MyDirectory_path, Toast.LENGTH_SHORT).show();
             PictureFileName = MyDirectory_path + "/MyPicture.jpg";
             camPreview.CameraTakePicture(PictureFileName);
+            Message message = new Message();
+            message.what = 1;
+            mHandler.sendMessage(message);
         }
     };
 
     private void initChart() {
+        dataSets = new ArrayList<>();
+        xVals = new ArrayList<>();
+
         LineChart chart = (LineChart) findViewById(R.id.chart);
         YAxis leftAxis = chart.getAxisLeft();
         LimitLine ll = new LimitLine(140f, "Critical Blood Pressure");
@@ -126,35 +154,43 @@ public class MyCamera extends Activity {
         xAxis.setDrawAxisLine(true);
         xAxis.setDrawGridLines(false);
 
-        ArrayList<Entry> valsComp1 = new ArrayList<Entry>();
-        Entry c1e1 = new Entry(100.000f, 0); // 0 == quarter 1
-        valsComp1.add(c1e1);
-        Entry c1e2 = new Entry(50.000f, 1); // 1 == quarter 2 ...
-        valsComp1.add(c1e2);
+        Float[] _float = {2.3f, 4.5f, 3.3f, 5.7f, 1.3f, 8.7f};
+        List<Float> list = new ArrayList<Float>();
+        Collections.addAll(list, _float);
+        setParameters(list);
 
-        LineDataSet setComp1 = new LineDataSet(valsComp1, "Company 1");
+        ArrayList<String> x = new ArrayList<String>();
+        x.add("1.Q");
+        x.add("2.Q");
+        x.add("3.Q");
+        x.add("4.Q");
+        x.add("5.Q");
+        x.add("6.Q");
+        setxVals(x);
 
-        ArrayList<LineDataSet> dataSets = new ArrayList<LineDataSet>();
-        dataSets.add(setComp1);
+        drawPlot();
+    }
 
-        ArrayList<String> xVals = new ArrayList<String>();
-        xVals.add("1.Q");
-        xVals.add("2.Q");
-        xVals.add("3.Q");
-        xVals.add("4.Q");
+    public void setParameters(List<Float> list) {
+        dataSets.clear();
+        ArrayList<Entry> valsComp = new ArrayList<Entry>();
+        for (int i = 0; i < list.size(); i++) {
+            valsComp.add(new Entry(list.get(i), i));
+        }
+        LineDataSet setComp = new LineDataSet(valsComp, "中心光谱");
+        dataSets.add(setComp);
+    }
 
+    public void setxVals(ArrayList<String> list) {
+        xVals.clear();
+        xVals = list;
+    }
+
+    public void drawPlot() {
         LineData data = new LineData(xVals, dataSets);
         chart.setData(data);
         chart.invalidate();
     }
 
-    public void setParameters(List<Float> list){
-        ArrayList<Entry> valsComp1 = new ArrayList<Entry>();
-        for(int i=0;i<list.size();i++){
-            valsComp1.add(new Entry(list.get(i),i));
-        }
-        LineDataSet setComp1 = new LineDataSet(valsComp1, "Company 1");
-       // dataSets.add(setComp1);
-    }
 
 }
